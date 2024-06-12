8533b5a08035a2e6c251d63ea7ac5064de6b518d
caioslopes <caiolopes.social@gmail.com>
2024-06-12 20:48:39 -0300

    updating Structuring

--- src/main/java/br/com/apollomusic/app/ApolloApplication.java
+++ src/main/java/br/com/apollomusic/app/ApolloApplication.java
@@ -1,4 +1,4 @@
-package br.com.apollo.app;
+package br.com.apollomusic.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/apollomusic/app/ApolloApplicationTests.java
+++ src/test/java/br/com/apollomusic/app/ApolloApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.apollo.app;
+package br.com.apollomusic.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
