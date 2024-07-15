ae6a6fa2d853aa11ccba2b7749e5c0c022a8b5cb
LucasAlt40 <lucas.alt40@gmail.com>
2024-07-15 20:02:04 -0300

    fix: remove id from user controller

--- src/main/java/br/com/apollomusic/app/controller/UserController.java
+++ src/main/java/br/com/apollomusic/app/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user/{userID}")
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
