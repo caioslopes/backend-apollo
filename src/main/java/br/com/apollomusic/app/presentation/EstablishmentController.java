0ad6de6a0c307b50a0001d1a87a53471febe909d
caioslopes <caiolopes.social@gmail.com>
2024-08-24 21:04:22 -0300

    refactor: removing test folder and test dependencies in pom.xml and import PreAuthorize

--- src/main/java/br/com/apollomusic/app/presentation/EstablishmentController.java
+++ src/main/java/br/com/apollomusic/app/presentation/EstablishmentController.java
@@ -6,6 +6,7 @@ import br.com.apollomusic.app.infra.config.JwtUtil;
 import br.com.apollomusic.app.domain.services.EstablishmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
