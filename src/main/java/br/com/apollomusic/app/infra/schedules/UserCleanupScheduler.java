1f7eba3d6f9915c0d183dd861b2740459765d6f6
LucasAlt40 <lucas.alt40@gmail.com>
2024-08-26 12:56:58 -0300

    fix: problem with reauthenticate api when run scheduler

--- src/main/java/br/com/apollomusic/app/infra/schedules/UserCleanupScheduler.java
+++ src/main/java/br/com/apollomusic/app/infra/schedules/UserCleanupScheduler.java
@@ -1,5 +1,6 @@
 package br.com.apollomusic.app.infra.schedules;
 
+import br.com.apollomusic.app.application.ApiAuthService;
 import br.com.apollomusic.app.domain.Establishment.Establishment;
 import br.com.apollomusic.app.application.EstablishmentService;
 import br.com.apollomusic.app.infra.repository.EstablishmentRepository;
@@ -12,10 +13,12 @@ public class UserCleanupScheduler {
 
     private final EstablishmentRepository establishmentRepository;
     private final EstablishmentService establishmentService;
+    private final ApiAuthService apiAuthService;
 
-    public UserCleanupScheduler(EstablishmentRepository establishmentRepository, EstablishmentService establishmentService) {
+    public UserCleanupScheduler(EstablishmentRepository establishmentRepository, EstablishmentService establishmentService, ApiAuthService apiAuthService) {
         this.establishmentRepository = establishmentRepository;
         this.establishmentService = establishmentService;
+        this.apiAuthService = apiAuthService;
     }
 
     @Scheduled(fixedRate = 3600000)
@@ -24,6 +27,7 @@ public class UserCleanupScheduler {
         Iterable<Establishment> establishments = establishmentRepository.findAll();
 
         for (Establishment establishment : establishments) {
+            apiAuthService.renewTokenIfNeeded(establishment.getOwner().getEmail());
             establishmentService.removeUsers(establishment.getId());
         }
     }
